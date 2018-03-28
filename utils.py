2eae21457f0c6170c2c5930ee5771c28034ea356
yselivonchyk <y.selivonchyk@gmail.com>
2018-03-28 14:23:04 -0700

    convert graph metadata to readable format

--- utils.py
+++ utils.py
@@ -15,3 +15,14 @@ def get_op(name):
   """
   if ':' in name: name = name.split(':')[0]
   tf.get_default_graph().get_operation_by_name(name) 
+
+
+def graph_meta_to_text(path, output=None):
+  """
+  Convert graph meta to text
+  """
+  if not output: output = path + 'txt'
+  with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+    tf.train.import_meta_graph(path)
+    tf.train.export_meta_graph(output, as_text=True)
+ 
