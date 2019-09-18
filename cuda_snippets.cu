3fbfab37279d93c4c7c4c341a387c5ee704f2c51
yselivonchyk <y.selivonchyk@gmail.com>
2019-09-17 20:53:28 -0700

    add cuda snippets

---
+++ cuda_snippets.cu
@@ -0,0 +1,33 @@
+// 
+// nvcc -arch=sm_70 -o out cuda.cu -run
+// !nvprof ./cuda
+
+// Error handling
+#include <stdio.h>
+#include <assert.h>
+
+inline cudaError_t checkCuda(cudaError_t result)
+{
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+  return result;
+}
+checkCuda( cudaDeviceSynchronize() );
+
+errSync  = cudaGetLastError();
+errAsync = cudaDeviceSynchronize(); // Wait for the GPU to finish
+if (errSync  != cudaSuccess) { printf("Sync  error: %s\n", cudaGetErrorString(errSync) ); }
+if (errAsync != cudaSuccess) { printf("Async error: %s\n", cudaGetErrorString(errAsync)); }
+
+
+// Properties
+int deviceId;
+cudaGetDevice(&deviceId);                 
+cudaDeviceProp props;
+cudaGetDeviceProperties(&props, deviceId);
+computeCapabilityMajor = props.major;
+computeCapabilityMinor = props.minor;
+multiProcessorCount = props.multiProcessorCount;
+warpSize = props.warpSize;
